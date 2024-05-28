8de72351b70284f321eb43571f85750d7c75dff3
Izaak-Grinner <kusk_0619@outlook.jp>
2024-05-28 19:33:21 +0900

    h

--- src/pages/SetDatabase.jsx
+++ src/pages/SetDatabase.jsx
@@ -7,7 +7,7 @@ const VideoOverlay = () => {
     return (
         <div>
             <video width='100%' controls>
-                <source src='src\timeShockTimer.mp4' type='video/mp4' />
+                <source src='../timeShockTimer.mp4' type='video/mp4' />
                 Your browser does not support the video tag.
             </video>
         </div>
