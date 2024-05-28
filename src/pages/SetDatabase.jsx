cf0c5f621c631eb2cc09c959ded5f2609eb924eb
Izaak-Grinner <kusk_0619@outlook.jp>
2024-05-29 00:17:19 +0900

    yyyyyyyyyyyyyy

--- src/pages/SetDatabase.jsx
+++ src/pages/SetDatabase.jsx
@@ -1,15 +1,36 @@
 //import React, { useRef, useEffect } from 'react';
 //import Webcam from 'react-webcam';
+import timeShockText from "../images/timeShockText.png";
+//import timeShockCamback from "../images/timeShockCamback.png";
+import timeShockTimer from "../videos/timeShockTimer.mp4";
 
 const VideoOverlay = () => {
+    const styleShow = {
+        position: 'relative',
+    };
+
+    const stylePosition = {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        margin: 'auto',
+        width: '100%'
+    };
 
 
     return (
         <div>
-            <video width='100%' controls>
-                <source src='../timeShockTimer.mp4' type='video/mp4' />
-                Your browser does not support the video tag.
-            </video>
+            <div className="showQuiz" style={styleShow}>
+                <video autoPlay style={stylePosition}>
+                    <source src={timeShockTimer} type='video/mp4' />
+                    Your browser does not support the video tag.
+                </video>
+
+                <img src={timeShockText} alt="failure" style={stylePosition} />
+
+
+            </div>
         </div>
     );
 };
